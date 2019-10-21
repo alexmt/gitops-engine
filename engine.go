a6ce8311345230e3fa1528cd88044682ba1be511
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2019-10-21 14:51:32 -0700

    Step 4. Move engine package into a separate repo

---
+++ engine.go
@@ -0,0 +1,45 @@
+package engine
+
+import (
+	"time"
+
+	"github.com/argoproj/argo-cd/engine/controller"
+
+	"github.com/argoproj/argo-cd/engine/pkg"
+	appv1 "github.com/argoproj/argo-cd/engine/pkg/apis/application/v1alpha1"
+	appclientset "github.com/argoproj/argo-cd/engine/pkg/client/clientset/versioned"
+	"github.com/argoproj/argo-cd/engine/util/kube"
+	"github.com/argoproj/argo-cd/engine/util/lua"
+)
+
+func NewEngine(
+	namespace string,
+	settingsMgr pkg.ReconciliationSettings,
+	db pkg.CredentialsStore,
+	auditLogger pkg.AuditLogger,
+	applicationClientset appclientset.Interface,
+	repoClientset pkg.ManifestGenerator,
+	argoCache pkg.AppStateCache,
+	appResyncPeriod time.Duration,
+	selfHealTimeout time.Duration,
+	metricsPort int,
+	kubectlParallelismLimit int64,
+	healthCheck func() error,
+	luaVMFactory func(map[string]appv1.ResourceOverride) *lua.VM,
+) (pkg.Engine, error) {
+	return controller.NewApplicationController(
+		namespace,
+		settingsMgr,
+		db,
+		auditLogger,
+		applicationClientset,
+		repoClientset,
+		argoCache,
+		&kube.KubectlCmd{},
+		appResyncPeriod,
+		selfHealTimeout,
+		metricsPort,
+		kubectlParallelismLimit,
+		healthCheck,
+		luaVMFactory)
+}
